9bf4562449baff66eb135502cbcb0c6f3cdb1bc4
KKaciusis <kkaciusis@yahoo.com>
2021-06-08 17:38:18 +0200

    variables

---
+++ script.js
@@ -0,0 +1,34 @@
+"use strict"
+
+var skaicius1, skaicius2, skaicius3, skaicius4, suma, sandauga;
+skaicius1 = 1;
+skaicius2 = 2;
+skaicius3 = 3;
+skaicius4 = 4;
+
+suma = skaicius1 + skaicius2 + skaicius3 + skaicius4;
+sandauga = skaicius1 * skaicius2 * skaicius3 * skaicius4;
+
+var temp = skaicius1, skaicius2
+skaicius1 = 2
+skaicius2 = 1
+
+var skaicius3, skaicius4
+skaicius3 = 4
+skaicius4 = 3
+
+var maxSkaicius;
+maxSkaicius = Number.MAX_VALUE
+
+alert("skaicius1 = 2")
+alert("skaicius2 = 1")
+alert("skaicius3 = 4")
+alert("skaicius4 = 3")
+
+console.log(skaicius1)
+console.log(skaicius2)
+console.log(skaicius3)
+console.log(skaicius4)
+console.log(suma)
+console.log(sandauga)
+console.log(maxSkaicius)
